c48382be0ad47fe7884c4211a4e96d60c40b8c0e
torianos <73795936+torianos@users.noreply.github.com>
2021-06-22 13:46:05 +0300

    Add files via upload

--- ToDoVita/app.js
+++ ToDoVita/app.js
@@ -1,10 +1,11 @@
-Vue.createApp({
-    data() {
-        return {
-            valueInput: '',
-            needDoList: [],
-            completeList: []
-        };
+var app = new Vue({
+    el: '#app',
+    data: {
+        valueInput: '',
+        needDoList: [],
+        completeList: [],
+        values: '',
+        namer: [],
     },
     methods: {
         handlyInput(event) {
@@ -18,7 +19,6 @@ Vue.createApp({
             });
             this.valueInput = '';
         },
-      
         doCheck(index, type) {
             if (type === 'need') {
                 const completeMask = this.needDoList.splice(index, 1);
@@ -34,4 +34,62 @@ Vue.createApp({
             toDoList.splice(index, 1);
         }
     }
-}).mount('#app');
+});
+new Vue({
+    el: '#app1',
+    data:  {
+        selected: true,
+        val: '',
+        comp: []
+    },
+    methods: {
+        toggleclass() {
+            this.selected = !this.selected
+        },
+        addquest() {
+            if (this.val === '') { return };
+            this.comp.push({
+                title: this.val,
+                id: Math.random()
+            });
+            this.val = '';
+            },
+    },
+    computed: {
+        textbtn() {
+            return this.selected ? 'Важная задача' : 'Обычная задача'
+        }
+    },
+
+})
+
+Vue.component('newtask1', {
+    data: function () {
+        return {
+            proverka: this.$root.selected}
+    },
+    template: '\
+      <li :class="{important : proverka}">\
+        {{ title }}\
+        <button v-on:click="$emit(\'remove\')">Удалить</button>\
+      </li>\
+    ',
+    props: ['title']
+  })
+
+
+  new Vue({
+    el: '#app2',
+      data: {
+          test: '',
+        numbers: [ "й", "ц", "у", "к", "е", "н" ]
+    },
+      methods: {
+          deltext() {
+           this.test = this.test.slice(0, -1);
+         }
+    },
+    computed: {
+    },
+
+  })
